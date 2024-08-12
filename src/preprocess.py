57b62beb2b9ab9c07d9b830d1877abb38a48dae0
KasaharaGen <gen.kasahara.124@gmail.com>
2024-08-12 14:29:09 +0900

    2024/8/10 first

--- src/preprocess.py
+++ src/preprocess.py
@@ -50,39 +50,3 @@ class preprocess_text:
         return jugde_dict
     
     
-class preprocess_df:
-    def __init__(self,columns):
-        self.columns=columns
-        return
-    
-    def first_make_df(self):
-        df=pd.DataFrame(columns=self.columns)
-        return df
-    
-    def make_labels_df(self,df,text):
-        new_data={}   
-        for column in self.columns:
-            new_data[column] = 1 if re.search(re.escape(column), text) else 0
-        label_df=pd.DataFrame([new_data])
-        df=pd.concat([df,label_df],ignore_index=True)
-        return df
-    
-    def comp_df(self,store_list):
-        Dengue_df=pd.concat(store_list,ignore_index=True)
-        Dengue_df.index.name='patientID'
-        return Dengue_df 
-    
-    def make_comp_df(self,one_words_Dengue_df,two_words_Dengue_df,three_words_Dengue_df,four_words_Dengue_df):
-        Dengue_df=pd.merge(one_words_Dengue_df,two_words_Dengue_df,on='patientID',how='left')
-        Dengue_df=pd.merge(Dengue_df,two_words_Dengue_df,on='patientID',how='left')
-        Dengue_df=pd.merge(Dengue_df,three_words_Dengue_df,on='patientID',how='left')
-        Dengue_df=pd.merge(Dengue_df,four_words_Dengue_df,on='patientID',how='left')
-        return Dengue_df
-
-    def make_csv(self,Dengue_df,save_path_name):
-        Dengue_df.to_csv(save_path_name,index=True)
-        return
-
-
-
-
--- src/running_preprocess.py
+++ src/running_preprocess.py
@@ -1,22 +1,20 @@
 import os
+import pandas as pd
 from preprocess import preprocess_text,preprocess_df
 
 dir_path='../data/dengue_data'
 
 pre_text=preprocess_text()
-#columns=(colomnsの単語数×それぞれのcolumns)の2次元配列
-columns=['cold','craniocaudal','mediolateral','fever','cough']  #システム上小文字で入力するのを忘れずに
-pre_df=preprocess_df(columns)
-pre_dengue_df=pre_df.first_make_df()
-store_list=[pre_dengue_df]
+
+extraction_words=['cold','craniocaudal','mediolateral','fever','cough']  #システム上小文字で入力するのを忘れずに
+dir_path='../data/dengue_data'
+dict_df=[]
 
 for filename in os.listdir(dir_path):
     pdf_path = os.path.join(dir_path, filename)
-
     text=pre_text.OCR(pdf_path)
+    jugde_dict=pre_text.extraction_words_to_dict(text,extraction_words)
+    dict_df.append(jugde_dict)
 
-    df=pre_df.make_labels_df(pre_dengue_df,text)
-    store_list.append(df)
+df=pd.DataFrame(dict_df)
 
-Dengue_df=pre_df.comp_df(store_list)
-pre_df.make_csv(Dengue_df,'../data/Dengue.csv')
